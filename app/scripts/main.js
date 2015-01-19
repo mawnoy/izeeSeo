cc6bbeade95fe7a983ae86d13bd8525cb2b8fac3
faisol <puff_dragon_6@hotmail.com>
2015-01-19 12:56:00 +0700

    navBar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+//************************//
+//   Preloading script
+//************************//
+
+$(window).load(function() {
+	'use strict';
+    setTimeout(function() {
+    $('.spinner').fadeOut('slow');
+            
+      setTimeout(function() {
+      $('#preloader').fadeOut('slow'); 
+            
+      }, 700);    
+          
+    }, 700);  
+       
+});
+
+//************************//
+//   NavBar
+//************************//
+
+$("#toggle").on("click", function() {
+	$(".row-offcanvas").toggleClass("active");
+});
